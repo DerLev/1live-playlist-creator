fbf9c46ebfa683fbd6a17148a64bb689509fedad
DerLev <levin.schroeren@gmail.com>
2024-03-25 11:40:27 +0100

    adding cloud functions baseline

---
+++ functions/src/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import {setGlobalOptions} from "firebase-functions/v2/options";
+
+setGlobalOptions({
+  maxInstances: 1,
+  memory: "128MiB",
+  region: "europe-west1",
+  timeoutSeconds: 20,
+});
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = onRequest((request, response) => {
+  logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
