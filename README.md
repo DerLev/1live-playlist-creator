ca6ea7158025575a9739c3e8244ec4fb23b2b9fe
DerLev <levin.schroeren@gmail.com>
2024-03-25 10:43:23 +0100

    finished proof-of-concept
    * POC can fetch the playlist from a specific hour and output it as an array

--- README.md
+++ README.md
@@ -3,8 +3,9 @@ An application for creating playlists from the German radio station 1LIVE
 
 ## Roadmap
 
-- [ ] Create a proof-of-concept
+- [x] Create a proof-of-concept
 - [ ] Create a GCP/Firebase project and translate POC into a working app (gather data as other parts get finished)
 - [ ] Add logic to interface with the Spotify API and create pre-made playlists on my personal account
 - [ ] Add a webapp (most likely React/Next.js) to give users more insights on the playlists (show gathered data)
 - [ ] Add a job for filling up the backlog
+- [ ] Expand to other 1LIVE plalists (Neu für den Sektor, DIGGI)
--- proof-of-concept/src/app.ts
+++ proof-of-concept/src/app.ts
@@ -2,9 +2,9 @@ import { JSDOM } from "jsdom"
 
 const main = async () => {
   const body = new URLSearchParams()
-  body.append("playlistSearch_date", "2024-03-24")
-  body.append("playlistSearch_hours", "19")
-  body.append("playlistSearch_minutes", "00")
+  body.append("playlistSearch_date", "2023-09-13")
+  body.append("playlistSearch_hours", "09")
+  body.append("playlistSearch_minutes", "30")
   body.append("submit", "suchen")
 
   const response = await fetch("https://www1.wdr.de/radio/1live/musik/playlist/index.jsp", {
@@ -27,14 +27,24 @@ const main = async () => {
     const artist = element.querySelector("td.performer")?.textContent?.replace(/\n/g, "")
     const played = element.querySelector("th.datetime")?.textContent?.replace(/\n/g, "")
 
+    if(!artist?.length || !played?.length || !title?.length) return
+
+    const formattedDate = played.replaceAll(",", ".").replace(" Uhr", "")
+    /* items in array are [day, month, year, hour, minute] */
+    const dateArray = formattedDate.split(".")
+    const dateISOString = `${dateArray[2]}-${dateArray[1]}-${dateArray[0]}T${dateArray[3]}:${dateArray[4]}:00.000Z`
+    const dateObject = new Date(Date.parse(dateISOString))
+
     return {
       title,
       artist,
-      played
+      played: dateObject
     }
-  })
+  }).filter((item) => item !== undefined) as {title: string, artist: string, played: Date}[]
 
-  console.log(formattedSearchResults)
+  const orderedSearchResults = formattedSearchResults.sort((a, b) => a.played.getTime() - b.played.getTime())
+
+  console.log(orderedSearchResults)
 }
 
 main()
