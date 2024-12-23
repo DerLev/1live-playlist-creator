c4ad9dee95d2d6fbc2366f64b4b60870b2d74679
DerLev <levin.schroeren@gmail.com>
2024-12-23 11:57:11 +0100

    adding custom favicon

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
         <ColorSchemeScript defaultColorScheme="dark" />
       </head>
       <body>
